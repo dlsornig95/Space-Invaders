ede01c90341f9eb6bafdda1dda28e68eb90b0d77
dlsornig95 <daniel.sornig@utechconsulting.net>
2026-03-13 14:06:27 -0400

    Remove mylocker-web (moved to separate repo)

--- mylocker-web/backend/api/__init__.py
+++
@@ -1,4 +0,0 @@
-from .conveyors import router as conveyors_router
-from .packages import router as packages_router
-from .websocket import router as websocket_router
-from .tags import router as tags_router
--- mylocker-web/backend/plc/__init__.py
+++
@@ -1,3 +0,0 @@
-from .connection import PLCConnection
-from .tags import read_tag, write_tag, read_multiple_tags
-from .config import PLC_CONFIG, MOCK_MODE
